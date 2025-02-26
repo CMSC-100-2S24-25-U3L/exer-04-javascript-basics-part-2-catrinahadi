22a0d182490ad6a5244ade8618ffc3e04f4caec6
catrinahadi <mohadi1@up.edu.ph>
2025-02-26 12:21:17 +0800

    updated readme file

---
+++ test.js
@@ -0,0 +1,7 @@
+import { addAccount } from './index.js'; // calls the function addAcount from index.js
+
+//testing
+addAccount(["Alan", "Turing", "aturing@w3c.com", 58]);
+addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]);
+addAccount(["Catrina", "Hadi", "mcatrina@gmail.com", 19]); 
+addAccount(["Catrina", "Hadi", "mcatrina@gmail.com", 15]); // will not be generated due to age (also tried testing with missing fields)
