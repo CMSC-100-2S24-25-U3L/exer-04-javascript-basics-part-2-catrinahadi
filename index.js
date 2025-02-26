73b41b2bddb869f5bd3e396deddc48fd19c71070
catrinahadi <mohadi1@up.edu.ph>
2025-02-26 12:14:46 +0800

    added generate unique id function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import { v4 as uuid } from 'uuid'; //import using es modules
 import validator from 'validator';
 import fs from 'fs';
+
+// function to generate a unique ID
+function generateUniqueID(firstName, lastName) {
+    const firstLetter = firstName.charAt(0).toLowerCase(); // gets first letter in lowercase
+    const lowerLastName = lastName.toLowerCase(); // converts last name to lowercase
+    const uuidWithoutHyphens = uuid().replace(/-/g, '').slice(0, 8); // removes hyphens & gets first 8 characters
+    const uniqueID = `${firstLetter}${lowerLastName}${uuidWithoutHyphens}`; // creates unique ID using string interpolation
+    return uniqueID; // returns the generated unique ID as a string
+}
