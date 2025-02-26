4ce9aa83d482acf5485e8886b54a3f38f230bfb0
catrinahadi <mohadi1@up.edu.ph>
2025-02-26 12:15:26 +0800

    added add account function

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function generateUniqueID(firstName, lastName) {
     const uniqueID = `${firstLetter}${lowerLastName}${uuidWithoutHyphens}`; // creates unique ID using string interpolation
     return uniqueID; // returns the generated unique ID as a string
 }
+
+// function to add account
+// export keyword so i can call this function in the test file
+export function addAccount(userInfo) {
+    const [firstName, lastName, email, age] = userInfo; //array of info
+    if (
+        userInfo.length === 4 && // all fields are present
+        firstName && lastName && email && // first name, last name, and email are non empty-strings
+        validator.isEmail(email) && // the email is in a valid format
+        typeof age === 'number' && age >= 18 // if age is a number, and at least 18
+    ) {
+        const uniqueID = generateUniqueID(firstName, lastName); // then generate unique id
+        const userData = `${firstName},${lastName},${email},${age},${uniqueID}\n`; // store into a data using string interpolation
+        try {
+            fs.appendFileSync('users.txt', userData); // save the data into a new line in users.txt file
+            return true;
+        } catch {
+            return false;
+        }
+    } else {
+        return false; // ret false if atleast one of the conditions is not met
+    }
+}
